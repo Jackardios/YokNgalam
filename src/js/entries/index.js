a7a1bc780dcc4ace1cbe14abb8c50bed89b1f0f5
Jackardios <salahutdinov.salavat@gmail.com>
2017-08-23 03:05:04 +0300

    Hero is done

--- src/js/entries/index.js
+++ src/js/entries/index.js
@@ -1,5 +1,42 @@
-import "../../scss/global.scss";
+import '../../scss/global.scss';
+import $ from 'jquery';
+
+function parallax(layers) {
+    var scrolled = $(window).scrollTop();
+    layers.forEach(function(layer) {
+        $(layer.element).css({"transform": "translate3d(0px, " + (scrolled * layer.value) + "px, 0px)"});
+    });
+}
 
 window.onload = function() {
     svgxuse;
+
+    // events
+    $('#profile-btn').click(function(e) {
+        $('.profile-menu', this).slideToggle(240);
+        return false;
+    });
+
+    // parallax effect
+    var layers = [
+        {
+            element: $("#hero-layer-0"),
+            value: 0.1
+        },
+        {
+            element: $("#hero-layer-1"),
+            value: 0.35
+        },
+    ];
+    parallax(layers);
+    $(window).scroll(function() {
+        parallax(layers);
+    });
+
+    // smooth scroll
+    $("body").on('click', '[href*="#"]', function(e){
+        $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top}, 1000);
+        e.preventDefault();
+    });
+
 }
